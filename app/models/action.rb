e3d9df8666125fe86f1bd36ebb49071ce67a97ff
Eduardo Martin Rojo <emr@mib107770i.internal.sanger.ac.uk>
2017-06-21 17:58:38 +0100

    Corrected scope

--- app/models/action.rb
+++ app/models/action.rb
@@ -4,8 +4,8 @@ class Action < ActiveRecord::Base
 
   @@TYPES = [:checkFacts, :addFacts, :removeFacts]
 
-  scope :include_subject_condition_group, ->() { includes(:subject_condition_group)}
-  scope :include_object_condition_groups, ->() { includes(:include_object_condition_groups)}
+  scope :include_subject_condition_groups, ->() { includes(:subject_condition_group)}
+  scope :include_object_condition_groups, ->() { includes(:object_condition_group)}
 
   def self.types
     @@TYPES
